ede8b58ba170cfea7c96bc610a856a6a4ba04112
Raza <razabinashraf@gmail.com>
2021-04-07 10:33:50 +0530

    Commit after completion

---
+++ ezed/calc/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request) :
+    return render(request, 'home.html', {'name' : 'MANU'})
+
+def add(request) :
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    return render(request, 'results.html', {'result' : val1+val2})
